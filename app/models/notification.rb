9d402879432f5d587f0ba2988c5b5e0ec4c5c2e0
Ales Guzik <me@aguzik.net>
2014-07-29 21:45:16 +0300

    added notification sample ordering

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,7 @@
 class Notification < ActiveRecord::Base
-  has_many :notification_samples, inverse_of: :notification, dependent: :destroy
+  has_many :notification_samples, inverse_of: :notification,
+                                   dependent: :destroy,
+                                       order: 'datetime DESC'
   has_many :payments
   has_many :notification_instances, through: :notification_samples
   after_initialize :init
